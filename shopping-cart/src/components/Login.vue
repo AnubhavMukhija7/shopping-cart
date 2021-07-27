4b5b03cc0da006d2c7a14801d5204d7756d22100
Anubhav Mukhija <amakhija@ex2india.com>
2021-07-27 16:48:51 +0530

    login: view done

---
+++ shopping-cart/src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>Log In</h2>
+        <form @submit.prevent ="loginHandler">
+            <input v-model="userName" placeholder="User Name" />
+            <br/>
+            <input v-model="password" type="password" placeholder="Password" />
+            <br/>
+            <button type=""> Login </button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            userName: '',
+            password: '',
+        }
+    },
+    methods:{
+        loginHandler() { 
+            const req = {
+                userName:this.userName,
+                password:this.password
+            }
+            console.log(req)
+        }
+    }
+}
+</script>
