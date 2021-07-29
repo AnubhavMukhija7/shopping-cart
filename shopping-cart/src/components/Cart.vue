a41228c7caa864b476d7854b2ddf294fe74b999a
Anubhav Mukhija <amakhija@ex2india.com>
2021-07-29 13:09:39 +0530

    no items in cart added in cart page

--- shopping-cart/src/components/Cart.vue
+++ shopping-cart/src/components/Cart.vue
@@ -1,83 +1,105 @@
 <template>
-    <div id="cart" class="cart-page">
-        <h2>Cart</h2>
-        <table id="product-details">
-            <tr>
-                <th>Product</th>
-                <th>Quantity</th>
-                <th>Size</th>
-                <th>Colour</th>
-                <th>Subtotal</th>
-            </tr>
-            <tr v-for="product in cartProducts" :key="product.id">
-                <td><div id="product-name">{{product.name}}</div></td>
-                <td><input type="number" v-model="product.quantity" @change="update"/></td>
-                <td><input type ="text" v-model="product.size" @change="update"/></td>
-                <td><div id = "product-colour">{{product.colour}}</div></td>
-                <td><div id="product-sub-total">{{product.quantity*product.price.value}} {{product.price.currency}}</div></td>
-                <td><button @click="deleteProduct(product.id)">delete this product</button></td>
-            </tr>
-        </table>
-        <div class="total-price">
-            <table id="price-details">
-                <tr>
-                    <td>subTotal</td>
-                    <td>{{subTotal}}</td>
-                </tr>
-                <tr>
-                    <td>tax:</td>
-                    <td>{{taxOnCart}}</td>
-                </tr>
-                <tr>
-                    <td>Total Cart Price</td>
-                    <td>{{totalCartPrice}}</td>
-                </tr>
-            </table>
-        </div>
-        <br>
-        <router-link :to="`/order`"><button >Place an Order</button></router-link>
+  <div id="cart" class="cart-page">
+    <h2>Cart</h2>
+    <br />
+    <div v-if="cartProducts.length < 1">
+      <p>You have no items in your cart</p>
+      <p>Add Items to cart first</p>
+      <router-link to="/items">Go To Home</router-link>
     </div>
+    <div v-else>
+      <table id="product-details">
+        <tr>
+          <th>Product</th>
+          <th>Quantity</th>
+          <th>Size</th>
+          <th>Colour</th>
+          <th>Subtotal</th>
+        </tr>
+        <tr v-for="product in cartProducts" :key="product.id">
+          <td>
+            <div id="product-name">{{ product.name }}</div>
+          </td>
+          <td>
+            <input type="number" v-model="product.quantity" @change="update" />
+          </td>
+          <td><input type="text" v-model="product.size" @change="update" /></td>
+          <td>
+            <div id="product-colour">{{ product.colour }}</div>
+          </td>
+          <td>
+            <div id="product-sub-total">
+              {{ product.quantity * product.price.value }}
+              {{ product.price.currency }}
+            </div>
+          </td>
+          <td>
+            <button @click="deleteProduct(product.id)">
+              delete this product
+            </button>
+          </td>
+        </tr>
+      </table>
+      <div class="total-price">
+        <table id="price-details">
+          <tr>
+            <td>subTotal</td>
+            <td>{{ subTotal }}</td>
+          </tr>
+          <tr>
+            <td>tax:</td>
+            <td>{{ taxOnCart }}</td>
+          </tr>
+          <tr>
+            <td>Total Cart Price</td>
+            <td>{{ totalCartPrice }}</td>
+          </tr>
+        </table>
+      </div>
+      <br />
+      <router-link :to="`/order`"><button>Place an Order</button></router-link>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import {CartItem, CartItems} from '../interface/cart-items-interace';
-import Vue from 'vue'
+import { CartItem, CartItems } from '../interface/cart-items-interace';
+import Vue from 'vue';
 export default Vue.extend({
-    methods:{
-        subTotalCalc(priceEach:number,quantity:number):number{
-            return priceEach*quantity;
-        },
-        deleteProduct(productId:number){
-            this.$store.dispatch('deleteProduct',productId);
-        },
-        update(){
-            this.$store.dispatch('updateProducts',this.cartProducts);
-        }
+  methods: {
+    subTotalCalc(priceEach: number, quantity: number): number {
+      return priceEach * quantity;
     },
-    computed:{
-        cartProducts(){
-            return this.$store.state.cart.cartItems;
-        },
-        subTotal(){
-            return this.$store.state.cart.subTotalPrice;
-        },
-        taxOnCart(){
-            return this.$store.state.cart.tax;
-        },
-        totalCartPrice(){
-            return this.$store.state.cart.totalPrice;
-        }
-    }
+    deleteProduct(productId: number) {
+      this.$store.dispatch('deleteProduct', productId);
+    },
+    update() {
+      this.$store.dispatch('updateProducts', this.cartProducts);
+    },
+  },
+  computed: {
+    cartProducts() {
+      return this.$store.state.cart.cartItems;
+    },
+    subTotal() {
+      return this.$store.state.cart.subTotalPrice;
+    },
+    taxOnCart() {
+      return this.$store.state.cart.tax;
+    },
+    totalCartPrice() {
+      return this.$store.state.cart.totalPrice;
+    },
+  },
 });
 </script>
 
 <style>
-.cart-page{
-    margin: 80px auto;
-
+.cart-page {
+  margin: 80px auto;
 }
-table{
-    width:100%;
-    border-collapse: collapse;
+table {
+  width: 100%;
+  border-collapse: collapse;
 }
 </style>
--- shopping-cart/src/store/modules/cart-module.ts
+++ shopping-cart/src/store/modules/cart-module.ts
@@ -1,49 +1,50 @@
-import { CartItems,CartItem } from "@/interface/cart-items-interace";
-import { Items } from "@/interface/items-interface";
-import { getItemsService } from "@/services/get-items-service";
+import { CartItems, CartItem } from '@/interface/cart-items-interace';
+import { Items } from '@/interface/items-interface';
+import { getItemsService } from '@/services/get-items-service';
 
-export default{
-    state:{
-        cartItems:[] as CartItems,
-        subTotalPrice : 0 as number,
-        tax: 0 as number,
-        totalPrice : 0,
+export default {
+  state: {
+    cartItems: [] as CartItems,
+    subTotalPrice: 0,
+    tax: 0,
+    totalPrice: 0,
+  },
+  mutations: {
+    ADD_ITEMS(state: any, payload: any) {
+      state.cartItems.push(payload);
     },
-    mutations:{
-        ADD_ITEMS(state:any,payload:any){
-            state.cartItems.push(payload);            
-        },
-        UPDATE_ITEMS(state:any,payload:any){
-                state.cartItems=payload;
-                let total=0;
-                const productArray= state.cartItems;
-                productArray.forEach((product:CartItem) => {
-                    if(product.quantity>0){
-                        total+=product.price*product.quantity;
-                    }else{
-                        alert('enter the quantity in positive value');
-                        product.quantity=1;
-                    }
-                });
-                console.log(total);
-                state.subTotalPrice=total;
-                state.tax = (12/100)*total;
-                state.totalPrice = state.subTotalPrice+state.tax;
-        },
-        DELETE_ITEMS(state:any,productId:number){
-            state.cartItems= state.cartItems.filter((product:CartItem) => product.id!== productId);
+    UPDATE_ITEMS(state: any, payload: any) {
+      state.cartItems = payload;
+      let total = 0;
+      const productArray = state.cartItems;
+      productArray.forEach((product: CartItem) => {
+        if (product.quantity > 0) {
+          total += product.price * product.quantity;
+        } else {
+          alert('enter the quantity in positive value');
+          product.quantity = 1;
         }
+      });
+      console.log(total);
+      state.subTotalPrice = total;
+      state.tax = (12 / 100) * total;
+      state.totalPrice = state.subTotalPrice + state.tax;
     },
-    actions:{
-        addItems(context:any,payload:any){
-            context.commit('ADD_ITEMS',payload)
-        },
-        updateProducts(context:any,payload:any){
-            context.commit('UPDATE_ITEMS',payload)
-        },
-        deleteProduct(context:any,productId:number){
-            context.commit('DELETE_ITEMS',productId);
-        }
+    DELETE_ITEMS(state: any, productId: number) {
+      state.cartItems = state.cartItems.filter(
+        (product: CartItem) => product.id !== productId
+      );
     },
-    
-}
+  },
+  actions: {
+    addItems(context: any, payload: any) {
+      context.commit('ADD_ITEMS', payload);
+    },
+    updateProducts(context: any, payload: any) {
+      context.commit('UPDATE_ITEMS', payload);
+    },
+    deleteProduct(context: any, productId: number) {
+      context.commit('DELETE_ITEMS', productId);
+    },
+  },
+};
